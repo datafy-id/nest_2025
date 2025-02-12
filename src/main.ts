a5127ac3d541387debb2d5e37deb4e80385c7204
Ridho <reedho@gmail.com>
2025-02-13 06:01:34 +0700

    Fix async bootstrap must be awaited

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+const logger = new Logger('Main');
+
+bootstrap()
+  .then(() => logger.log(`BOOTSTRAP_SUCCESS`))
+  .catch((error) => {
+    logger.error(`BOOTSTRAP_ERROR ${error}`);
+  });
